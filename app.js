f458709a4eabab460d63c8db5f67c459a4627f1b
flavius <kete102@gmail.com>
2023-11-22 14:53:07 +0100

    Updated html layout and js code

--- app.js
+++ app.js
@@ -1,49 +1,100 @@
 console.log("Welcome to the Rock Paper Scissors Game!");
 
+// Player Buttons
+const playerRockButton = document.getElementById("player-rock-button");
+const playerPaperButton = document.getElementById("player-paper-button");
+const playerScissorsButton = document.getElementById("player-scissors-button");
+const playerButtons = document.querySelectorAll(".player-option");
+
+//Computer Buttons
+const computerRockButton = document.getElementById("computer-rock-button");
+const computerPaperButton = document.getElementById("computer-paper-button");
+const computerScissorsButton = document.getElementById(
+  "computer-scissors-button"
+);
+
+//Scoreboard
+const playerWinsScoreboard = document.getElementById("player-game-wins");
+const computerWinsScoreboard = document.getElementById("computer-game-wins");
+
 const game = () => {
   console.log("Let's play!");
+
   let playerWins = 0;
   let computerWins = 0;
 
-  while (playerWins < 3 && computerWins < 3) {
-    const computerChoice = getComputerChoice();
-    const playerChoice = getPlayerChoice();
-
-    playRound(playerChoice, computerChoice);
-  }
-
   function getComputerChoice() {
+    [computerRockButton, computerPaperButton, computerScissorsButton].forEach(
+      (button) => {
+        button.classList.remove("computer-button-selected");
+      }
+    );
+
     const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
     const choice = choices[randomIndex];
-    console.log("Computer's choice is " + choice);
+
+    //Color computer's choice
+    if (choice === "rock") {
+      computerRockButton.classList.add("computer-button-selected");
+    }
+    if (choice === "paper") {
+      computerPaperButton.classList.add("computer-button-selected");
+    }
+    if (choice === "scissors") {
+      computerScissorsButton.classList.add("computer-button-selected");
+    }
     return choice;
   }
 
-  function getPlayerChoice() {
-    //let choice = prompt("Please choose rock, paper, or scissors: ");
-    console.log("Your choice is " + choice.toLowerCase());
-    return choice.toLowerCase();
+  function getPlayersChoice(button) {
+    playerButtons.forEach((button) => {
+      button.classList.remove("player-button-selected");
+    });
+
+    button.classList.add("player-button-selected");
+    const choice = button.getAttribute("value");
+    const computerChoice = getComputerChoice();
+    playRound(choice, computerChoice);
   }
 
+  playerButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const selectedButton = event.target;
+      getPlayersChoice(selectedButton);
+    });
+  });
+
   function playRound(playerChoice, computerChoice) {
+    console.log("Computer's choice is " + computerChoice);
+    console.log("Player's choice is " + playerChoice);
+
     if (
       (playerChoice === "rock" && computerChoice === "scissors") ||
       (playerChoice === "paper" && computerChoice === "rock") ||
       (playerChoice === "scissors" && computerChoice === "paper")
     ) {
-      console.log("You win this round!");
+      console.log("Player wins this round!");
       playerWins++;
-    } else if (playerChoice === computerChoice) {
-      console.log("It's a tie!");
-    } else {
-      console.log("You lose this round!");
+      playerWinsScoreboard.textContent = playerWins;
+    } else if (
+      (playerChoice === "scissors" && computerChoice === "rock") ||
+      (playerChoice === "rock" && computerChoice === "paper") ||
+      (playerChoice === "paper" && computerChoice === "scissors")
+    ) {
+      console.log("Computer wins this round!");
       computerWins++;
+      computerWinsScoreboard.textContent = computerWins;
+    } else {
+      console.log("It's a tie!");
     }
-    console.log(`Player: ${playerWins} - Computer: ${computerWins}`);
-  }
 
-  console.log(`The winner is ${playerWins === 3 ? "player" : "computer"}.`);
+    if (playerWins === 5) {
+      console.log("Player wins the game!");
+    } else if (computerWins === 5) {
+      console.log("Computer wins the game!");
+    }
+  }
 };
 
 game();
